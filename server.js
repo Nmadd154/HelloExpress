b50b9e578e8fa61bdb403493bf38ddc462cfa49a
Nicholas Maddox <maddoxn@my.lanecc.edul>
2025-11-14 20:32:14 -0800

    add name

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!\n");
+  res.send("Hello Nick!\n");
   console.log("refreshed");
 });
 
